bd2546556c5e926aee7e2cbede5f04ec0ece03fb
Dhiman-Chandra-Ray <prdhimanchandra@gmail.com>
2025-07-03 00:44:51 +0600

    second commit

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../layout/Root";
+import Carousel from "../components/Carousel/Carousel";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        Component: Root
+        Component: Root,
+        children: [
+            {
+                path: "/",
+                Component: Carousel
+            }
+        ]
     }
 ]);
 
