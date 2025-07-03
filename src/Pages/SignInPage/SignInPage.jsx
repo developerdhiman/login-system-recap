b203499c0a6eacbaf432e71879fba6fd10d7092b
Dhiman-Chandra-Ray <prdhimanchandra@gmail.com>
2025-07-03 12:49:15 +0600

    setup firebase

--- src/Pages/SignInPage/SignInPage.jsx
+++ src/Pages/SignInPage/SignInPage.jsx
@@ -10,7 +10,7 @@ const SignInPage = () => {
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
-          </p>
+          </p>  
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
