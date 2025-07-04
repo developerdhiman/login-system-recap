4bf13da5bb2fd555370c18d230c04ad70eacf620
Dhiman-Chandra-Ray <prdhimanchandra@gmail.com>
2025-07-04 19:02:39 +0600

    problem solve

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">DhimanBD24</a>
+        <Link to="/" className="btn btn-ghost text-xl">DhimanBD24</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
